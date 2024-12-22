c2ee8550cdc94af2e94ef1586367df6ba823db6f
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-22 18:04:16 +0300

    feat: update SignIn screen with new layout and Google sign-in functionality

--- app/SignIn.tsx
+++ app/SignIn.tsx
@@ -1,11 +1,45 @@
-import { View, Text } from "react-native";
+import { Image, ScrollView, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>Sign In</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.signIn}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-8">
+          <Text className="text-base font-afacadFlux text-black text-center">
+            WELCOME TO KICKS
+          </Text>
+          <Text className="text-3xl font-afacadFlux-extrabold text-black text-center mt-2">
+            Let's Get You the {"\n"}
+            <Text className="text-3xl font-afacadFlux-extrabold text-primary-300 text-center mt-2">
+              Perfect Pair
+            </Text>
+          </Text>
+          <TouchableOpacity
+            className="mt-8 flex items-center gap-4 flex-row justify-center border border-gray-200 rounded-full py-3 shadow-md"
+            onPress={handleLogin}
+          >
+            <Image
+              source={icons.google}
+              className="h-6 w-6"
+              resizeMode="contain"
+            />
+            <Text className="text-lg font-afacadFlux-bold text-black text-center">
+              Sign in with Google
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
