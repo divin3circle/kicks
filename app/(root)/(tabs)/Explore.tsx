8ec110c82638db60c4ef85154d8e90a0f0dc4689
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-22 15:23:54 +0300

    feat: add initial project structure with nativewind and expo integration

---
+++ app/(root)/(tabs)/Explore.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Explore = () => {
+  return (
+    <View>
+      <Text>Explore</Text>
+      <Text className="text-2xl text-red-600 font-bold">Home</Text>
+    </View>
+  );
+};
+
+export default Explore;
