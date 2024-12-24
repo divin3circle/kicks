20112510a1ebd276e93d673c6fd4eca4fc1d3b7d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-24 11:34:54 +0300

    feat: implement logout functionality with user feedback and dynamic profile display

--- app/(root)/(tabs)/Profile.tsx
+++ app/(root)/(tabs)/Profile.tsx
@@ -5,23 +5,31 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import React from "react";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
+import { useGlobalContext } from "@/lib/GlobalProvider";
+import { logout } from "@/lib/appwrite";
 
 type IoniconsName = keyof typeof Ionicons.glyphMap;
 
 interface ProfileItemProps {
+  id: number;
   title: string;
   iconName: IoniconsName;
+  onPress?: () => void;
 }
 
-function ProfileItem({ title, iconName }: ProfileItemProps) {
+function ProfileItem({ title, iconName, onPress, id }: ProfileItemProps) {
   return (
-    <TouchableOpacity className="flex-row items-center justify-between w-full p-3">
+    <TouchableOpacity
+      className="flex-row items-center justify-between w-full p-[10px]"
+      onPress={onPress}
+    >
       <View className="flex-row items-center gap-2">
         <Ionicons
           name={iconName}
@@ -44,7 +52,7 @@ function ProfileItem({ title, iconName }: ProfileItemProps) {
 }
 
 const Profile = () => {
-  const profileItems = [
+  const profileItems: ProfileItemProps[] = [
     {
       id: 1,
       title: "My Orders",
@@ -86,66 +94,111 @@ const Profile = () => {
       iconName: "person-add",
     },
   ];
-  async function handleLogout() {}
+  const { user, refetch } = useGlobalContext();
+  async function handleLogout() {
+    const result = await logout();
+    if (result) {
+      Alert.alert("Logout", "You have been logged out successfully", [
+        {
+          text: "Ok",
+          onPress: () => {
+            refetch();
+          },
+        },
+      ]);
+    } else {
+      Alert.alert("Logout", "An error occurred while logging out", [
+        {
+          text: "Ok",
+        },
+      ]);
+    }
+  }
   return (
     <SafeAreaView className="h-full bg-white dark:bg-black">
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerClassName="pb-38 px-7"
-      >
-        <View className="flex-row justify-between items-center mt-5">
-          <Text className="text-3xl font-afacadFlux-extrabold text-black dark:text-white">
-            Profile
-          </Text>
-          <Ionicons name="notifications" size={24} color="#000" />
-        </View>
-        <View className="flex-row justify-center flex mt-5">
-          <View className="flex flex-col items-center mt-5 relative">
-            <Image source={images.avatar} className="size-44 rounded-full" />
-            <TouchableOpacity className="absolute right-2 bottom-10">
-              <Image source={icons.edit} className="size-9" />
-            </TouchableOpacity>
-            <Text className="text-2xl font-afacadFlux-bold text-black mt-3">
-              Sylus Abel
+      {user ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerClassName="pb-38 px-7"
+        >
+          <View className="flex-row justify-between items-center mt-5">
+            <Text className="text-3xl font-afacadFlux-extrabold text-black dark:text-white">
+              Profile
             </Text>
+            <Ionicons name="notifications" size={24} color="#000" />
           </View>
-        </View>
-        <View className="my-2">
-          {profileItems.splice(0, 2).map((item) => (
-            <ProfileItem
-              key={item.id}
-              title={item.title}
-              iconName={item.iconName}
-            />
-          ))}
-        </View>
-        <View className="my-2">
-          {profileItems.splice(2).map((item) => (
-            <ProfileItem
-              key={item.id}
-              title={item.title}
-              iconName={item.iconName}
-            />
-          ))}
-          <TouchableOpacity className="flex-row items-center justify-between w-full p-4">
-            <View className="flex-row items-center gap-2">
-              <Ionicons name="log-out" size={24} color="#ff0000" />
-              <Text className="text-2xl font-afacadFlux-bold text-danger">
-                Logout
+          <View className="flex-row justify-center flex mt-5">
+            <View className="flex flex-col items-center mt-5 relative">
+              <Image
+                source={{
+                  uri: user?.avatar,
+                }}
+                className="size-44 rounded-full"
+              />
+              <TouchableOpacity className="absolute right-2 bottom-10">
+                <Image source={icons.edit} className="size-9" />
+              </TouchableOpacity>
+              <Text className="text-2xl font-afacadFlux-bold text-black mt-3">
+                {user?.name}
               </Text>
             </View>
-          </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center mt-5 pb-16">
+          </View>
+          <View className="my-2">
+            {profileItems.splice(0, 2).map((item) => (
+              <ProfileItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                iconName={item.iconName}
+              />
+            ))}
+          </View>
+          <View className="my-2">
+            {profileItems.splice(2).map((item) => (
+              <ProfileItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                iconName={item.iconName}
+                onPress={() => {}}
+              />
+            ))}
+            <TouchableOpacity
+              className="flex-row items-center justify-between w-full p-4"
+              onPress={handleLogout}
+            >
+              <View className="flex-row items-center gap-2">
+                <Ionicons name="log-out" size={24} color="#ff0000" />
+                <Text className="text-2xl font-afacadFlux-bold text-danger">
+                  Logout
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center mt-5 pb-16">
+            <Link
+              href="/"
+              className="underline font-afacadFlux-light py-3 px-5"
+              onPress={() => {}}
+            >
+              App Version 1.0.0
+            </Link>
+          </View>
+        </ScrollView>
+      ) : (
+        <View className="flex flex-col items-center justify-center h-full">
+          <Text className="text-2xl font-afacadFlux-bold text-black dark:text-white">
+            You are not logged in
+          </Text>
           <Link
-            href="/"
-            className="underline font-afacadFlux-light py-3 px-5"
+            href="/SignIn"
+            className="underline font-afacadFlux-light py-3 px-5 mt-5"
             onPress={() => {}}
           >
-            App Version 1.0.0
+            Signin
           </Link>
         </View>
-      </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
